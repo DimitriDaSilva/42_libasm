f31c2cf807d68374ea9aa5bf84f5ed597ae46228
Dimitri Da silva <dda-silv@c1r16s6.42lisboa.com>
2021-03-26 11:39:22 +0000

    Doing strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -9,26 +9,19 @@ _ft_strcmp:
 	; Setting return all bytes of reg to 0 because we'll be working with al
 	mov		rax, 0
 	; Loop starts at -1 but gets incremented directly so it starts at 0
-	mov		rbx, -1
-	call	cmp_chars
+	mov		rbx, 0
+	call	.loop
 
 	; Epilogue
 	pop		rbx
 	ret
 	
-cmp_chars:
-	inc		rbx
-
+.loop:
 	; Get the value of src char
 	mov		al, byte [rdi + rbx]
 	; Get the difference between the two into the al register
 	sub		al, byte [rsi + rbx]
 
-	cmp		byte [rdi + rbx], 0x00
-	je
-
-
-	cmp		al, 0x00
-	je		cmp_chars
+	jz		.loop
 	ret
 
