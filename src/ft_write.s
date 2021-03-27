ecaf4f28643b361888a6ba9bec1587bcc3e7400c
Dimitri Da silva <dda-silv@c2r2s2.42lisboa.com>
2021-03-27 12:35:23 +0000

    Trying to set errno in ft_write

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,8 +1,20 @@
 default rel ; set RIP-relative addressing to default
 
+extern ___error
+
 global _ft_write
 
 _ft_write:
+	
 	mov		rax, 0x2000004
 	syscall
+	test	rax, rax
+	js		.error
+	ret
+
+.error:
+	;mov		rdi, rax
+	mov		byte [rax], -1
+	call	___error
+	mov		byte [rax], 9
 	ret
