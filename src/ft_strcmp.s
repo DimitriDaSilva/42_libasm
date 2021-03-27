e7ecd3fd2369c034f43b2af0750062d35b074a95
Dimitri Da silva <dda-silv@c2r2s4.42lisboa.com>
2021-03-27 08:53:25 +0000

    First honnest try at strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -7,21 +7,40 @@ _ft_strcmp:
 	push	rbx
 
 	; Setting return all bytes of reg to 0 because we'll be working with al
-	mov		rax, 0
+	xor		rax, rax
+
 	; Loop starts at -1 but gets incremented directly so it starts at 0
-	mov		rbx, 0
-	call	.loop
+	mov		rbx, -1
+	call	.cmp_chars
 
 	; Epilogue
 	pop		rbx
 	ret
 	
-.loop:
-	; Get the value of src char
+.cmp_chars:
+	inc		rbx
+	; Get the difference between the two into the al register (lower byte of rax)
 	mov		al, byte [rdi + rbx]
-	; Get the difference between the two into the al register
 	sub		al, byte [rsi + rbx]
 
-	jz		.loop
-	ret
+	xor		rcx, rcx	; Init to 0
+	xor		r8d, r8d	; Check if end of string
 
+	; Value that will be moved if condition met
+	; Not possible to use cmove & all cmov variants with immediates
+	mov		rdx, 1
+
+	; Branchless compound if statement
+	; Check if no difference between chars
+	cmp		al, 0
+	cmove	rcx, rdx	; Move 1 into rcx if al == 0
+	; Check if end of string. No need to check rsi because previous condition
+	; checks if both are equal
+	cmp		byte [rdi + rbx], 0
+	cmove	r8, rdx		; Move 1 into r8 if dest char is NULL
+	
+	and		rcx, r8		; Check if both conditions are met
+
+	cmp		rcx, 1		;
+	je		.cmp_chars
+	ret
