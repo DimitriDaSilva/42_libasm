505134cadeeb15c1b6f90e0f2b12a9a20c8245de
dda-silv <dda-silv@student.42lisboa.com>
2021-03-29 15:30:13 +0100

    Auto-commit from dda-silv@c2r2s2 on Mon Mar 29 15:30:13 WEST 2021

--- tester/src/bonus/ft_atoi_base_test.c
+++ tester/src/bonus/ft_atoi_base_test.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 12:50:26 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/03/29 15:27:37 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/03/29 15:28:57 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@ void	ft_atoi_base_test(void)
 	print_header("ft_atoi_base");
 
 	check(ft_atoi_base("    +++12", 0) == 0);
+	check(ft_atoi_base("    +++12", "") == 0);
+	check(ft_atoi_base("    +++12", "1") == 0);
 }
