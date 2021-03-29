73fd96f338a98b2f7fc41ce26331ce0994238b68
dda-silv <dda-silv@student.42lisboa.com>
2021-03-29 16:42:20 +0100

    Started ft_strchr_bonus

--- src/ft_strchr_bonus.s
+++ src/ft_strchr_bonus.s
@@ -1,14 +1,18 @@
 		default	rel						; Set RIP-relative addressing to default
 
+		extern	_ft_strlen
+
 		global	_ft_strchr
 
 _ft_strchr:
 		mov		rax, rdi
 
-		cmp		byte rsi,
+		cmp		byte rsi, 0
+		call	_ft_strlen
 
 .check_if_null:
 		cmp		byte [rdi], 0
 		jz		.is_null
 
-
+.exit:
+		ret
