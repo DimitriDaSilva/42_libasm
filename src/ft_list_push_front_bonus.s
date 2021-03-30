659cb3823da622a5608eecaa19e11086c905d68c
dda-silv <dda-silv@student.42lisboa.com>
2021-03-30 16:11:25 +0100

    Started setup work on linked lists

--- src/ft_list_push_front_bonus.s
+++ src/ft_list_push_front_bonus.s
@@ -2,10 +2,16 @@
 
 		global	_ft_list_push_front
 
-section	.data
+		section	.data
+
+struc	s_list
+	data_long:	resd	1
+endstruc
+	
+		section	.bss
 
 
-section	.text
+		section	.text
 ;void ft_list_push_front(t_list **begin_list, void *data);
 ; begin_list	-> rdi
 ; data			-> rsi
