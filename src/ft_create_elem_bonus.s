e1cfeec73c94d84b7f6287319b1e0fd4901438b0
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2021-03-31 14:54:27 +0100

    Doing ft_create_elem

--- src/ft_create_elem_bonus.s
+++ src/ft_create_elem_bonus.s
@@ -1,4 +1,17 @@
+; t_list *ft_create_elem(void *data);
+		global	_ft_create_elem
 		default	rel						; Set RIP-relative addressing to default
 
-		global	_ft_create_elem
+		section	.data
+
+struc	s_list
+		data:	resq	1
+		next:	resq	1
+endstruc
+
+		section	.bss
+llist:	resq	1	
+
+		section	.text
+_ft_create_elem:
 
--- src/ft_list_push_front_bonus.s
+++ src/ft_list_push_front_bonus.s
@@ -1,24 +1,11 @@
+		global	_ft_list_push_front
 		default	rel						; Set RIP-relative addressing to default
 
-		global	_ft_list_push_front
-
-struc	s_list
-	data:		resq	1
-	next:		resb	s_list_size
-endstruc
-
-		section	.data
-
-struct:
-	istruc	s_list
-		at	data, dq ?
-		at	next, db ?
-	iend
 
 		section	.text
 ;void ft_list_push_front(t_list **begin_list, void *data);
 ; begin_list	-> rdi
 ; data			-> rsi
 _ft_list_push_front:
-		lea		rdx, [rdi]
+		mov rax, 1
 
