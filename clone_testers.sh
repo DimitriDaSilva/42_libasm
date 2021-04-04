95848c4cc9ef267165af70647a14c53108df30ee
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2021-04-04 09:38:17 +0100

    Started ft_remove_if_bonus

--- clone_testers.sh
+++ clone_testers.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/vscabell/42libasm_tester tester_vscabell
 git clone https://github.com/Tripouille/libasmTester tester_tripouille
-git clone https://github.com/cacharle/libasm_test.git ../tester_cacharle
 git clone https://github.com/mli42/mlibasm_tester tester_mli42
-git clone https://github.com/mjacq42/tester_libasm ../tester_mjacq42
--- src/ft_list_remove_if_bonus.s
+++ src/ft_list_remove_if_bonus.s
@@ -1,6 +1,10 @@
-;void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+;void	ft_list_remove_if(t_list **begin_list,
+;							void *data_ref,
+;							int (*cmp)(),
+;							void (*free_fct)(void *));
 
 		global	_ft_list_remove_if
+		extern _free
 		default	rel								; Set RIP-relative addressing to default
 
 		section	.data
@@ -14,5 +18,41 @@ endstruc
 
 		section	.text
 _ft_list_remove_if:
-		ret
+		; Prologue
+		push	rbx
+		mov		rbx, rsp
 
+		; Save begin_list to other scratch registers to free up rdi for the function calls
+		mov		r8, rdi					; begin_list
+
+		test	r8, r8					; Check if being_list is NULL
+		jz		.exit					; If null, return 0
+		mov		r8, [r8]				; Set r8 as the first node
+
+.parse_list:
+		; Check if head is NULL
+		test	r8, r8					
+		jz		.exit
+
+		; Compare data. rsi already holds the pointer to the data ref (2nd arg)
+		mov		rdi, qword [r8 + data]	; Set 1st arg for cmp
+		call	rdx						; rdx holds the address of the cmp function
+		test	rax, rax				
+		jnz		.continue				; If ret == 0, remove from list
+
+		; Else remove node
+		; 1st step: free data
+		call	rcx						; rcx holds the address of the free_fct
+
+		; 2st step: 
+
+
+
+.continue:
+		mov		r8, qword [r8 + next]	; Else continue with next node
+		jmp		.parse_list
+
+.exit:
+		mov		rsp, rbx
+		pop		rbx
+		ret
